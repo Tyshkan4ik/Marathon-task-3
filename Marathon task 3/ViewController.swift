52dfb6e5730b49629e1e85bd0c07822cd0ac9da6
Tyshkan4ik <001vat@gmail.com>
2023-03-07 12:27:10 +0300

    start the project

---
+++ Marathon task 3/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Marathon task 3
+//
+//  Created by Виталий Троицкий on 07.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .yellow
+        
+    }
+
+
+}
+
